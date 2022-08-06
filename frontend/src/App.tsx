6ee259bc96c2efd22b2a7b265351af4a69fd61e8
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-08-06 16:12:17 +0200

    Import a SolidJS scaffol

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { Component, createEffect, createSignal } from "solid-js";
+
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  const [first, setFirst] = createSignal("JSON");
+  const [last, setLast] = createSignal("Bourne");
+
+  createEffect(() => console.log(`${first()} ${last()}`));
+
+  return (
+    <div class={styles.App}>
+      <input
+        type="text"
+        value={first()}
+        onInput={(e) => setFirst((e.target as HTMLInputElement).value)}
+      />
+      <input
+        type="text"
+        value={last()}
+        onInput={(e) => setLast((e.target as HTMLInputElement).value)}
+      />
+    </div>
+  );
+};
+
+export default App;
